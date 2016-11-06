ed8f9a2906251f9ab260c91ef1a06a24658889f8
Lukas Knudsen Høgh <lukas@imf.au.dk>
2016-11-06 21:50:47 +0100

    change to config style

--- src/Mulligan/MulliganStrategy.ts
+++
@@ -1,9 +0,0 @@
-import Card from "../Card";
-import Side from "../Side";
-
-interface MulliganStrategy {
-    setMulligans(side: Side, cards: Card[]): void;
-    performMulligans(sides: Side, drawCard: (side: Side) => void): void;
-}
-
-export default MulliganStrategy;

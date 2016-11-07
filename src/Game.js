518540e59067679963786540c19c467169269ab1
Lukas Knudsen Høgh <lukas@imf.au.dk>
2016-11-07 16:36:14 +0100

    Moving to JSON configuration

--- src/Game.js
+++ src/Game.js
@@ -2,7 +2,6 @@
 var seedrandom = require("seedrandom");
 var RandomNumberGenerator_1 = require("./NumberGenerator/RandomNumberGenerator");
 var Utilities_1 = require("./Utilities");
-var _ = require("underscore");
 var Game = (function () {
     function Game(players) {
         /**
@@ -10,28 +9,11 @@ var Game = (function () {
          */
         this.seedlength = 64;
         this.setupRNG();
-        this.sides = [];
-        for (var p in players) {
-            this.sides.push(this.createSide(players[p]));
-        }
     }
     Game.prototype.setupRNG = function () {
         this.seed = Utilities_1.getRandomString(this.seedlength);
         this.prng = seedrandom(this.seed);
         this.randomNumberGenerator = new RandomNumberGenerator_1["default"](this.prng);
-    };
-    Game.prototype.createSide = function (player) {
-        var side = {
-            player: player,
-            deck: null,
-            hand: []
-        };
-        return side;
-    };
-    Game.prototype.getSideFromPlayer = function (player) {
-        return _.find(this.sides, function (side) {
-            return side.player == player;
-        });
     };
     return Game;
 }());
--- src/Game.ts
+++ src/Game.ts
@@ -1,7 +1,6 @@
 import Player from "./Player";
 import CardPile from "./CardPile";
 import Card from "./Card";
-import Side from "./Side";
 import * as seedrandom from "seedrandom";
 import RandomNumberGenerator from "./NumberGenerator/RandomNumberGenerator";
 import NumberGenerator from "./NumberGenerator/NumberGenerator";
@@ -19,38 +18,17 @@ export default class Game {
     /**
      * Fields:
      */
-    private sides: Side[];
     private seed: string;
     private prng: prng;
     private randomNumberGenerator: NumberGenerator;
 
     public constructor(players: Player[]) {
         this.setupRNG();
-
-        this.sides = [];
-        for (let p in players) {
-            this.sides.push(this.createSide(players[p]));
-        }
     }
 
     private setupRNG(): void {
         this.seed = getRandomString(this.seedlength);
         this.prng = seedrandom(this.seed);
         this.randomNumberGenerator = new RandomNumberGenerator(this.prng);
-    }
-
-    private createSide(player: Player): Side {
-        let side: Side = {
-            player: player,
-            deck: null,
-            hand: []
-        }
-        return side;
-    }
-    
-    private getSideFromPlayer(player: Player): Side {
-        return _.find(this.sides, function (side) {
-            return side.player == player;
-        });
     }
 }
